182dbe566c6aa17c987b4c94c7a2743bc2474cf6
OvO1111 <o.o111@sjtu.edu.cn>
2024-05-09 11:20:50 +0800

    some changes

--- ldm/modules/losses/lpips.py
+++ ldm/modules/losses/lpips.py
@@ -67,7 +67,7 @@ class LPIPS(nn.Module):
             param.requires_grad = False
 
     def load_from_pretrained(self, name="vgg_lpips"):
-        ckpt = get_ckpt_path(name, "/mnt/lustrenew/hukeyi/lwh/dlr/dependencies/vgg16-397923af.pth")
+        ckpt = get_ckpt_path(name, "/mnt/lustrenew/hukeyi/lwh/dlr/ldm/dependency/vgg16-397923af.pth")
         self.load_state_dict(torch.load(ckpt, map_location=torch.device("cpu")), strict=False)
         print("loaded pretrained LPIPS loss from {}".format(ckpt))
 
